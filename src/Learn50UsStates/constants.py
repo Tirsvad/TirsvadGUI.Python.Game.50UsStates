0a9bbb7382eaa81d252a690c011eaaa6dd02d68c
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2024-10-11 19:00:43 +0800

    Add visual studio solution Add python to exe

--- src/Learn50UsStates/constants.py
+++ src/Learn50UsStates/constants.py
@@ -1,3 +1,6 @@
-STATES_FILE = "data/50_states.csv"
-STATES_MISSED_FILE = "data/missed_state.csv"
-STATES_IMAGE_FILE = "images/blank_states_img.gif"
+import os
+
+STATES_FILE = "data" + os.path.sep + "50_states.csv"
+STATES_MISSED_FILE = "data" + os.path.sep + "missed_state.csv"
+STATES_IMAGE_FILE = "images" + os.path.sep + "blank_states_img.gif"
+
